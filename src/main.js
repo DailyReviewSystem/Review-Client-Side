f3dc0b23e4b1801f24b9945e247a419f66a2dee9
Leyiang <yiangforwork@gmail.com>
2022-01-16 21:14:30 +0800

    Index

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+
+/**
+ * Starter Template
+ */
 import App from './App.vue'
+
+/**
+ * Router File
+ */
+import router from "./router.js";
+
+/**
+ * Import LuLu UI
+ */
 import "./assets/ui.css"
 
-createApp(App).mount('#app')
+createApp(App)
+    .use( router )
+    .mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="greeting">
+    <span>Hi, Scofield</span>
+    <br>
+    <span>You have <router-link to="/">2</router-link> forms</span>
+  </h2>
+
+  <button class="ui-button" type="primary">现在填表</button>
+</template>
