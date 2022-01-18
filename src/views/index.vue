c187b6c8b9fcdb1120db22b3310971bc965489f5
Leyiang <yiangforwork@gmail.com>
2022-01-19 03:50:23 +0800

    Separate doneList & activeList && little style tweaking

--- src/views/index.vue
+++ src/views/index.vue
@@ -16,16 +16,43 @@ main.content {
   <main class="content">
     <div class="content-width">
 
-      <h3 class="list-title">未填表单 ({{ list.length }})</h3>
+      <section>
+        <h3 class="list-title">未填表单 ({{ activeList.length }})</h3>
 
-      <div class="flex col form-list">
-        <router-link
-            v-for="item in list"
-            :to="'/form/' + item.id"
-            class="ui-button"
-            style="display: block"
-        >{{ item.name }} ({{ item.date }})</router-link>
-      </div>
+        <div class="flex col form-list">
+          <template v-if="activeList.length === 0">
+            <span>You complete all forms, good for you!</span>
+          </template>
+
+          <template v-else>
+            <router-link
+                v-for="item in activeList"
+                :to="'/form/' + item.id"
+                class="ui-button"
+                style="display: block"
+            >{{ item.name }} ({{ item.date }})</router-link>
+          </template>
+        </div>
+      </section>
+
+      <section style="margin-top: 4rem;">
+        <h3 class="list-title">已完成表单 ({{ doneList.length }})</h3>
+
+        <div class="flex col form-list">
+          <template v-if="doneList.length === 0">
+            <span>No completed form being found.</span>
+          </template>
+
+          <template v-else>
+            <router-link
+                v-for="item in doneList"
+                :to="'/form/' + item.id"
+                class="ui-button"
+                style="display: block"
+            >{{ item.name }} ({{ item.date }})</router-link>
+          </template>
+        </div>
+      </section>
 
     </div>
   </main>
@@ -33,12 +60,14 @@ main.content {
 
 <script setup>
 
-import {inject, ref} from "vue";
-
-let list = ref([]);
+import {inject, ref, computed} from "vue";
 
 const api = inject("api");
 const title = inject("title");
+
+const list = ref([]);
+const doneList = computed(() => list.value.filter(item => item.done) );
+const activeList = computed(() => list.value.filter(item => ! item.done) );
 
 api.get("/user/forms").then( ({data}) => {
   list.value = data.data;
